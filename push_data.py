f1399398bb0790ac68e41cf6664ff2716c3ca3d2
PiyushBhavsar22 <piyushbhavsar.you@gmail.com>
2025-02-12 22:14:06 +0530

    push_data

--- push_data.py
+++ push_data.py
@@ -24,7 +24,7 @@ class NetworkDataExtract():
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def cv_to_json_convertor(self,file_path):
+    def csv_to_json_convertor(self,file_path):
         try:
             data=pd.read_csv(file_path)
             data.reset_index(drop=True,inplace=True)
@@ -47,3 +47,13 @@ class NetworkDataExtract():
             return(len(self.records))
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+        
+if __name__=='__main__':
+    FILE_PATH="Network_Data\phisingData.csv"
+    DATABASE="PIYUSHAI"
+    Collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
+    print(no_of_records)
