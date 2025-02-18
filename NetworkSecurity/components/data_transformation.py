b55a88cd26521481895e96643018fe90c27386a2
PiyushBhavsar22 <piyushbhavsar.you@gmail.com>
2025-02-19 00:43:49 +0530

    data transformation

--- NetworkSecurity/components/data_transformation.py
+++ NetworkSecurity/components/data_transformation.py
@@ -17,3 +17,38 @@ from NetworkSecurity.entity.config_entity import DataTransformationConfig
 from NetworkSecurity.exception.exception import NetworkSecurityException 
 from NetworkSecurity.logging.logger import logging
 from NetworkSecurity.utils.main_utils.utils import save_numpy_array_data,save_object
+
+class DataTransformation:
+    def __init__(self,data_validation_artifact:DataValidationArtifact,
+                 data_transformation_config:DataTransformationConfig):
+        try:
+            self.data_validation_artifact:DataValidationArtifact=data_validation_artifact
+            self.data_transformation_config:DataTransformationConfig=data_transformation_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def initiate_data_transformation(self)->DataTransformationArtifact:
+        logging.info("Entereed the initiate_data_transformation method of DataTransformation class")
+        try:
+            logging.info("Start data transformation")
+            train_df=DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df=DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+
+            ## Training Dataframe
+            input_feature_train_df=train_df.drop(columns=[TARGET_COLUMN],axis=1)
+            target_feature_train_df=train_df[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
+
+            ## Testing Dataframe
+            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(-1, 0)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
