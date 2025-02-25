5bf6da3eb7dcbe44df2c42e638bd371c3e6cb45a
PiyushBhavsar22 <piyushbhavsar.you@gmail.com>
2025-02-25 23:53:29 +0530

    estimator.py

--- NetworkSecurity/utils/ml_utils/model/estimator.py
+++ NetworkSecurity/utils/ml_utils/model/estimator.py
@@ -4,3 +4,17 @@ from NetworkSecurity.logging.logger import logging
 import os
 import sys
 
+class NetworkModel:
+    def __init__(self, preprocessor, model):
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def predict(self,x):
+        try:
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
